57c9c895bcb38b4fbb047e5268b5deac3b833cfa
Tim <tim@woodpigeon.com>
2012-03-14 09:07:48 +0000

    setup

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :gemcutter
+
+gem 'sinatra'
+gem 'thin'
+gem 'will_paginate'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'data_mapper'
+gem 'dm-sqlite-adapter'
+gem 'haml'
+gem 'json'
+gem 'heroku'
---
+++ main.rb
@@ -0,0 +1,64 @@
+require "rubygems"
+require "sinatra"
+require "json"
+require "data_mapper"
+require 'will_paginate'
+require 'will_paginate/collection'
+require 'will_paginate/view_helpers'
+
+DataMapper.setup(:default, "sqlite3:unicodes.db")
+
+class Character
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :code, String
+  property :hex, String
+  property :ref, String
+  property :description, String
+  
+  def self.paginated_search(params, term)
+    
+    per_page = params[:per_page] || 20
+    page = params[:page] || 1
+    
+    page = page.to_i
+    page -= 1
+    
+    per_page = per_page.to_i
+
+    offset = per_page * page
+
+    characters = Character.all
+
+    # apply search term is there is one
+    characters = characters.all({:description.like => "%#{term}%"}) unless term.empty?
+
+    # get the current set of characters to display
+    characters_on_this_page = characters.all(:limit => per_page, :offset => offset)
+
+    return WillPaginate::Collection.create(page + 1, per_page, characters.count) {|p| p.replace characters_on_this_page }
+   
+  end
+  
+end
+
+#helpers Pagination::Helpers
+
+get '/characters' do
+  @term = params[:term] || ''
+  @characters = Character.paginated_search(params, @term)
+  
+  # render /views/index.haml
+  haml :index, :format => :html5
+  
+end
+
+
+get '/characters.json' do
+  @term = params[:term] || ''
+  @characters = Character.paginated_search(params, @term)
+  content_type :json
+  @characters.to_json
+
+end
