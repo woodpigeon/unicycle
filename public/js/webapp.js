34b69f2cdef618fc270a3ccd32ae9e9818e3cbd1
Tim <tim@woodpigeon.com>
2012-03-15 10:27:51 +0000

    play

---
+++ public/js/webapp.js
@@ -0,0 +1,41 @@
+
+getData = function(search_term) {
+	
+	// Get the initial json data to display
+	$.get('/characters.json?term=' + search_term, function(data) {
+
+			entryIndex = 0;
+	    entries = data['characters'];
+  
+	    // compile Handlebars template
+	    var source = $("#chars_template").html();
+	    var template = Handlebars.compile(source);
+	    var entriesList = $("ul#chars");
+	    entriesList.html('');
+
+	    for (var i in entries) {
+	      entry = entries[i];
+	      var newItem = template(entry);
+				console.log(entry);
+	      entriesList.append(newItem);
+	    }
+   
+		});
+}
+
+$(function() {
+	
+	getData('');
+	
+	// wire up the form so not submit but use ajax
+	$("#myform").submit(function(a,b) {
+	
+		getData($("#term").val());
+		return false
+	});
+		
+	$("#term").bind('keyup', function() {
+		
+		getData($("#term").val());
+	})
+});
