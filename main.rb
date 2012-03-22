14768093c0996d4b53a27bfb95f41d1df89531e7
Tim <tim@woodpigeon.com>
2012-03-22 19:21:00 +0000

    docs, tweaks

--- main.rb
+++ main.rb
@@ -9,19 +9,11 @@ require 'will_paginate/view_helpers'
 require 'haml'
 require 'active_attr'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:unicodes.db")
-#DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://localhost/unicodes')
+# Sqlite data source using the DataMapper ORM
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:unicodes.db") # 'mysql://localhost/unicodes'
 
-class Pagination
-  include ActiveAttr::Model
-  attribute :next_page
-  attribute :previous_page
-  attribute :offset
-  attribute :current_page
-  attribute :total_pages
-  attribute :total_entries
-end
 
+# A model representing a unicode character, maps to a characters table in the database
 class Character
   include DataMapper::Resource
   
@@ -31,6 +23,7 @@ class Character
   property :ref, String
   property :description, String
   
+  # Helper method to search and return an array of characters paginated usin the will_paginate gem
   def self.paginated_search(params, term)
     
     per_page = params[:per_page] || 20
@@ -60,20 +53,53 @@ end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
+
+# This root url delivers a json-driven page with no server-side view formatting at all
 get '/' do
-  "ok - just a test ;-)"
+  haml :webapp, :format => :html5
 end
 
 
+# This url delivers a server-side page using haml
 get '/characters' do
   @term = params[:term] || ''
   @characters = Character.paginated_search(params, @term)
   haml :index, :format => :html5
 end
 
-
+# This url is a web service serving up characters as paginated json
+# This is the format of the json returned
+# {
+#     "characters": 
+#       [
+#         {
+#           "code": " ",
+#           "id": 1,
+#           "hex": "U+0020",
+#           "description": "SPACE",
+#           "ref": "&amp;#32;"
+#           },
+#           {...}
+#       ],
+#     "pagination": {
+#         "total_entries": 11293,
+#         "current_page_number": {
+#             "name": "page",
+#             "_dc_obj": 1
+#         },
+#         "total_pages": 565,
+#         "previous_page": "",
+#         "next_page": "http://unicycle.woodpigeon.com/characters.json?page=2&term=",
+#         "offset": 0,
+#         "current_page": "http://unicycle.woodpigeon.com/characters.json?page=1&term="
+#     }
+# }
 get '/characters.json' do
+  
+  content_type :json
+  
   @term = params[:term] || ''
+  
   @characters = Character.paginated_search(params, @term)
 
   pagination = { :next_page => url_for_page(@characters.next_page, @term), 
@@ -83,15 +109,12 @@ get '/characters.json' do
                               :offset => @characters.offset,
                               :total_entries =>  @characters.total_entries,
                               :total_pages => @characters.total_pages}
-  content_type :json
+  
   {:characters => @characters, :pagination => pagination}.to_json
 
 end
 
-get '/webapp' do
-  haml :webapp, :format => :html5
-end
-
+# Helper to format urls returned in the pagination json
 def url_for_page(page, term)
   if page.nil?
     return ''
